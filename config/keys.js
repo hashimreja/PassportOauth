ad2fa0b0f1adc607ecbf69c626b3538cc601b075
Syed Hashim Reja <hashimreja2@gmail.com>
2019-09-19 20:34:36 +0530

    changed the original key ids  to duplicate

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 module.exports ={
     google :{
-        clientID:'601392114650-b4kk1328q1gjg14sigeed21m4f3argel.apps.googleusercontent.com',
-        clientSecret: '6dbU42bLLe5T5M2z6elgd5cL'
+        clientID:'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+        clientSecret: 'xxxxxxxxxxxxxxxxxxxxxxxx'
     }
 }
